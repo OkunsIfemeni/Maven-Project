c879156ed27cfa580d0822d43b717b2fc2591c00
David Ifemeni <davidifemeni@gmail.com>
2023-03-02 03:02:22 -0600

    update appservice

--- Terraform/appservice.tf
+++ Terraform/appservice.tf
@@ -1,5 +1,5 @@
 resource "azurerm_service_plan" "maven-appservice" {
-  name                = "maven-appserviceplan"
+  name                = "maven-appservice-plan"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
